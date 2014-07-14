30561b7ca0c969e4e47a33791e391299c59d9959
Joey Bergeron <joey.bergeron@gmail.com>
2014-07-14 00:34:47 -0500

    Merge branch 'master' into gh-pages

---
+++ 23_Homework-modified/assets/javascript.js
@@ -0,0 +1,37 @@
+setInterval(function clock() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.querySelector('.clock').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(function() {
+        clock()
+    }, 500);
+
+    function colorChanger() {
+        var d = today.getTime() / (1000 * 60 * 60 * 24)
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        return "rgb(" +
+            (((d % 2) == 1) ? parseInt((h / 24) * 256) : (255 - parseInt((h / 24) * 256))) + "," +
+            (((h % 2) == 1) ? parseInt((m / 60) * 256) : (255 - parseInt((m / 60) * 256))) + "," +
+            (((m % 2) == 1) ? parseInt((s / 60) * 256) : (255 - parseInt((s / 60) * 256))) + ")"
+
+    };
+    $('.centered').css('color', colorChanger)
+    var rgbFix = colorChanger().replace("rgb(", "").replace(")","");    
+    console.log(rgbFix);
+    $('.color').html(rgbFix)
+    $('.shadow').css('textShadow','0px 0px 10px', colorChanger);
+}, 1000);
+
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
+    return i;
+};
